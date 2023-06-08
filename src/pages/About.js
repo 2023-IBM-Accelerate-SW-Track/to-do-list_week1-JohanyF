e4cf6aae0791114b9a86a59b7eb230fcf63f121f
JohanyF <johanyf627@gmail.com>
2023-06-08 18:52:09 -0500

    added about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,19 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img className="profile_image" alt="Profile Pic"></img>
+          </div>
+        </div> 
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Johany Flores</div>
+            <div className="brief_description">
+              I am a rising Junior attending the University of Texas at Arlington. 
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
